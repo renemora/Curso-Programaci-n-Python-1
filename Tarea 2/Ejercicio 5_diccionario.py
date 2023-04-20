6ebf91a2b804740544312a2d0c4d6788eec7f774
Rene Mora-Casal <rene.moracasal@ucr.ac.cr>
2023-04-19 19:57:26 -0600

    Ejercicio #5 terminado

--- Tarea 2/Ejercicio 5_diccionario.py
+++ Tarea 2/Ejercicio 5_diccionario.py
@@ -1,17 +1,31 @@
 # Ejercicio 6 - manipulación de un diccionario.
+# Primero defino unas variablas llamadas "nombre_estudiante" y "promedio_notas".
+# Les doy valores por medio del diccionario origen.
+# Finalmente creo el diccionario solicitado.
 
 # Definición del diccionario origen.
 
 sampleDict = {
-    "class": {
+    'class': {
         "student": {
             "name": "Mike",
             "marks": {
                 "physics": 70,
                 "history": 80,
-                "math: 90
+                "math": 90
             },
         }
     }
 }
 
+nombre_estudiante = sampleDict["class"]["student"]["name"]
+print("El nombre del estudiante es: ", nombre_estudiante)
+
+promedio_notas = (sampleDict["class"]["student"]["marks"]["physics"] + sampleDict["class"]["student"]["marks"]["history"] + sampleDict["class"]["student"]["marks"]["math"])/3
+print("Su promedio de notas es: ", promedio_notas)
+
+nuevoDict = { 
+    "nombre": nombre_estudiante, 
+    "promedio notas": promedio_notas 
+}
+print(nuevoDict)

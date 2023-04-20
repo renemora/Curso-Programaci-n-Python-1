0f2047dd2d4caec907cb6126695c1b34105b427d
Rene Mora-Casal <rene.moracasal@ucr.ac.cr>
2023-04-19 18:32:49 -0600

    Versión inicial Ejercicio 6 (solo diccionario 1)

---
+++ Tarea 2/Ejercicio 5_diccionario.py
@@ -0,0 +1,17 @@
+# Ejercicio 6 - manipulación de un diccionario.
+
+# Definición del diccionario origen.
+
+sampleDict = {
+    "class": {
+        "student": {
+            "name": "Mike",
+            "marks": {
+                "physics": 70,
+                "history": 80,
+                "math: 90
+            },
+        }
+    }
+}
+
